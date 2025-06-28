c3993cbc06e666d9d031fe7c2662518b21147c2d
davidlessard <ddavidlessard@gmail.com>
2025-06-28 16:04:58 -0400

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../globals.css'
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        {children}
+    </main>
+  );
+}
