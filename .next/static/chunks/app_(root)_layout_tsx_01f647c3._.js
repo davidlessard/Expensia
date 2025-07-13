fb6efd085a69b379bd9a6452e0715ec8315f4131
David Lessard <ddavidlessard@gmail.com>
2025-07-13 18:13:13 -0400

    Restore .gitignore and remove unwanted files from repo

--- .next/static/chunks/app_(root)_layout_tsx_01f647c3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_00475e9b._.js",
-  "static/chunks/_bf8d8bb0._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_(root)_page_tsx_5907067f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_a7694303._.js",
-  "static/chunks/components_5e0925bd._.js"
-],
-    source: "dynamic"
-});
